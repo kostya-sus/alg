db67d966c7e1fe900ef38265d95d1a7ede4fe199
Khok Shokhanul <schohan430@gmail.com>
2016-10-30 22:05:56 +0200

    1

---
+++ tasks/KhokHW/StringParser/StringParser/Dictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringParser
+{
+    class Dictionary
+    {
+        public string[] GetWords()
+        {
+            string str = ReadFromFile();
+            List<string> dictionary = new List<string>();
+            string temp = string.Empty;
+
+            int i = 0;
+            int size = str.Length;
+            while(i < size)
+            {
+                if (str[i] == '\'' || char.IsLetter(str[i]) || char.IsDigit(str[i]))
+                {
+                    temp += str[i];
+                }
+                else if(temp.Length != 0)
+                {
+                    dictionary.Add(temp);
+                    temp = string.Empty;
+                }
+                ++i;
+            }
+
+            if (temp.Length != 0)
+                dictionary.Add(temp);
+
+            return dictionary.ToArray();
+        }
+
+        public string ReadFromFile()
+        {
+            StreamReader sr = File.OpenText("dict_en.txt");
+            return sr.ReadToEnd();
+        }
+    }
+}
---
+++ tasks/KhokHW/StringParser/StringParser/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary d = new Dictionary();
+            string[] dictionary = d.GetWords();
+            Console.WriteLine("Enter string.");
+            string str = Console.ReadLine();
+            WordDivider w = new WordDivider(str, dictionary);
+            w.WordsBrake();
+        }
+    }
+}
