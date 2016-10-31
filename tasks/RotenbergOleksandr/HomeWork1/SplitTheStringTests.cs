0b152edfcff116185a7a0185eb50792cdbd99987
alexrrrr <robinzon.alex@gmail.com>
2016-10-31 22:23:46 +0200

    Add homework1(Split the string).

---
+++ tasks/RotenbergOleksandr/HomeWork1/SplitTheStringTests.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using SplitTheString;
+using Xunit;
+
+namespace SplitTheStringTests
+{
+    public class SplitTheStringTests
+    {
+
+        [Fact]
+        public void ShouldReturnTwoCombinations()
+        {
+            //Arrange
+            FindAllVariants fav = new FindAllVariants();
+            HashSet<string> dictionary = new HashSet<string>(){ "cat", "cats", "and", "sand", "dog" };
+            string inputText = "catsanddog";
+            List<string> expectedResult = new List<string>() { "cat sand dog", "cats and dog" };
+
+            //Act
+            var result = fav.FindSeparateWords(inputText, dictionary);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void ShouldReturnEmptyList()
+        {
+            //Arrange
+            FindAllVariants fav = new FindAllVariants();
+            HashSet<string> dictionary = new HashSet<string>() { "cat", "cats", "and", "sand", "dog" };
+            string inputText = "";
+            List<string> expectedResult = new List<string>() { };
+
+            //Act
+            var result = fav.FindSeparateWords(inputText, dictionary);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void ShouldReturn220DifferentValues()
+        {
+            //Arrange
+            FindAllVariants fav = new FindAllVariants();
+            HashSet<string> dictionary = new HashSet<string>(File.ReadAllLines(@"Dictionary/dict_en.txt"));
+            string inputText = "catsanddog";
+            int expectedResult = 220;
+
+            //Act
+            var result = fav.FindSeparateWords(inputText, dictionary);
+
+            //Assert
+            Assert.Equal(expectedResult, result.Count);
+        }
+    }
+}
