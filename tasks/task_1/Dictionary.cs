4a81fb70938369b199a41c39d1e6423b7ed51907
kostya-sus <kostya-sus@yandex.ru>
2016-11-01 00:24:01 +0200

    HW
    task1 task2

---
+++ tasks/task_1/Dictionary.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace HW1_ALG
+{
+    class Dictionary
+    {
+        public string[] ParseDictionary()
+        {
+            string wordpull = ReadFile();
+            string temp = string.Empty;
+            List<string> dictionary = new List<string>();
+            
+
+            int i = 0,
+                size = wordpull.Length;
+
+            while (i < size)
+            {
+                if (wordpull[i] == '\'' || char.IsLetter(wordpull[i]) || char.IsDigit(wordpull[i]))
+                {
+                    temp += wordpull[i];
+                }
+                else if (temp.Length != 0)
+                {
+                    dictionary.Add(temp);
+                    temp = string.Empty;
+                }
+                i++;
+            }
+
+            if (temp.Length != 0)
+                dictionary.Add(temp);
+
+            return dictionary.ToArray();
+        }
+
+
+        public string ReadFile()
+        {
+            StreamReader pull = File.OpenText("dict_en.txt");
+            return pull.ReadToEnd();
+        }
+    }
+}
---
+++ tasks/task_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace HW1_ALG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary d = new Dictionary();
+            string[] dictionary = d.ParseDictionary();
+
+            Console.WriteLine("Enter statement");
+            string input = Console.ReadLine();
+
+            WordDivider w = new WordDivider(input, dictionary);
+            w.WordsEmpty();
+            Console.ReadLine();
+        }
+    }
+}
